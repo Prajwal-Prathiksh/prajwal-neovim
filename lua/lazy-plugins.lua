0dd733329c677b617ce3993d72b0ab881bc1e748
Prajwal-Prathiksh <prajwal.prathiksh@gmail.com>
2024-09-06 13:01:01 +0530

    Update init

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -43,7 +43,14 @@ require('lazy').setup({
   require 'kickstart/plugins/mini',
 
   require 'kickstart/plugins/treesitter',
+  
+  require 'kickstart.plugins.indent_line',
 
+  require 'kickstart.plugins.lint',
+
+  require 'kickstart.plugins.autopairs',
+
+  require 'kickstart.plugins.nvim-tree',
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
@@ -54,10 +61,6 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
