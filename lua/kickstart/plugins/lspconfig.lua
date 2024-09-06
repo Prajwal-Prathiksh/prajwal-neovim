af6f5bfc7d4d04be10bf2e50a64121f5701b5105
Prajwal-Prathiksh <prajwal.prathiksh@gmail.com>
2024-09-06 13:00:33 +0530

    Install python specific LSPs

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -209,6 +209,9 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'isort', -- Used to format Python code
+        'black', -- Used to format Python code
+        'pylint', -- Used to lint Python code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
