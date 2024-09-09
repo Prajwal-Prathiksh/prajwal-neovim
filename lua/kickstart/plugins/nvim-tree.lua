b00d79d21104aa6cbaeea68eb7a218ecdb5499a6
Prajwal-Prathiksh <prajwal.prathiksh@gmail.com>
2024-09-09 20:07:18 +0530

    Add keybinding for copy file path

--- lua/kickstart/plugins/nvim-tree.lua
+++ lua/kickstart/plugins/nvim-tree.lua
@@ -52,5 +52,7 @@ return {
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    -- Copy file path to clipboard
+    keymap.set("n", "<leader>ep", "<cmd>let @*=expand('%:p')<CR>", { desc = "Copy file path to clipboard" })
   end
 }
